eabb1dfa3f894b57baac2a96ac97a121ba2e9d59
tcseley <tiborcseley@gmail.com>
2021-04-27 14:13:43 -0500

    updating core files

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-let cards = []
+let allCards = [...document.querySelectorAll(".card")]
+let firstClick
+let secondClick
+
+console.log(allCards)
+
+document.querySelector("#card-container").addEventListener("click", function(e){
+    if (firstClick === undefined) {
+        console.log("firstClick")
+        firstClick = e.target
+    } else if (firstClick !== undefined && secondClick === undefined) {
+        console.log("secondClick")
+        secondClick = e.target
+        console.log(firstClick.classList["1"] === secondClick.classList["1"])
+        firstClick = undefined;
+        secondClick = undefined;
+    
+    } 
+    
+    // else if (firstClick !== undefined && secondClick !== undefined) {
+    //     firstClick = undefined;
+    //     secondClick = undefined;
+    // }
+})
 
