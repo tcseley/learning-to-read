16d7bcb1059f26504a9708241dcd88c9490431c1
tcseley <tiborcseley@gmail.com>
2021-04-27 19:25:52 -0500

    added notes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let allCards = [...document.querySelectorAll(".card")]
 let firstClick
 let secondClick
+let score = 0
 
 // console.log(allCards)
 
@@ -16,13 +17,26 @@ document.querySelector("#card-container").addEventListener("click", function(e){
         secondClick = undefined;
     } 
     
+
     // else if (firstClick !== undefined && secondClick !== undefined) {
     //     firstClick = undefined;
     //     secondClick = undefined;
     // }
 })
 
+function scoreCount() {
+    if (firstClick.classList["1"] === secondClick.classList["1"]) {
+        score += 5
+    }
+    console.log(scoreCount)
+}
 //write function for card match on line 14
 //what happens next? point?
-//Think aobut how this is working ... is this true? and then is this true? Line by line, what's happening.
+//Think about how this is working ... is this true? and then is this true? Line by line, what's happening.
 //Game engine almost there.
+
+//The journey continues...place true and false statement is winning condition (create as function)
+//then add score to said function
+//display score somewhere readable
+//Instruction to play
+//shuffle cards (Math.random - will this work? Or do I <i>really</i> need an array?)
